49de0c0debe835cf6fb1b0e5aa370aa4c2a5a354
noireko <agustinfernandezrolon@gmail.com>
2026-04-15 14:51:57 -0300

    Added DOM to index.js

---
+++ src/index.js
@@ -0,0 +1,5 @@
+const title = document.getElementById("title");
+const desc = document.getElementById("desc");
+const date = document.getElementById("date");
+const btnSubmit = document.getElementById("btn-submit");
+const divTareas = document.getElementById("div-tareas")
