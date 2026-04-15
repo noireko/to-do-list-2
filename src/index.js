9a2c46263aa834ebad5f376704e5f96b86d309f9
noireko <agustinfernandezrolon@gmail.com>
2026-04-15 15:32:37 -0300

    Added import class and event listener logic

--- src/index.js
+++ src/index.js
@@ -3,3 +3,18 @@ const desc = document.getElementById("desc");
 const date = document.getElementById("date");
 const btnSubmit = document.getElementById("btn-submit");
 const divTareas = document.getElementById("div-tareas")
+
+import "./style.css"
+import toDoItem from "./toDos.js"
+
+btnSubmit.addEventListener("click", () => {
+    const tarea = new toDoItem(
+        title.value,
+        desc.value,
+        date.value
+    );
+    tarea.crearToDo(divTareas);
+    title.value = "";
+    desc.value = "";
+    date.value = "";
+})
